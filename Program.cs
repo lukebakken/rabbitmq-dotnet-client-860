f9d56b7b38c740c6b3005f98e8221dd6e2562ca3
Luke Bakken <luke@bakken.io>
2024-04-25 08:50:08 -0700

    https://github.com/rabbitmq/rabbitmq-dotnet-client/issues/860

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace repro
+{
+    class Program
+    {
+        const int threadCount = 32;
+        const int count = 20;
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(threadCount, threadCount);
+
+            var factory = new ConnectionFactory
+            {
+                AutomaticRecoveryEnabled = true,
+                TopologyRecoveryEnabled = true,
+                HostName = "ravel",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/",
+                RequestedConnectionTimeout = new TimeSpan(0, 0, 2),
+                UseBackgroundThreadsForIO = false,
+                DispatchConsumersAsync = true
+            };
+
+            var connection = factory.CreateConnection();
+
+            Console.WriteLine("Creating channels...");
+            var totalTicksCounter = Environment.TickCount;
+
+            var tasks = new Task[count];
+            for (int i = 0; i < count; ++i)
+            {
+                tasks[i] = Task.Factory.StartNew(
+                    obj => {
+                        var ticksCounter = Environment.TickCount;
+                        var channel = connection.CreateModel();
+                        var cost = Environment.TickCount - ticksCounter;
+                        Console.WriteLine($"ID:{(int)obj}, Cost:{cost:N0}ms.");
+                    }, i);
+            }
+
+            Task.WhenAll(tasks).Wait();
+            var totalCost = Environment.TickCount - totalTicksCounter;
+            Console.WriteLine($"Done, Total Cost:{totalCost:N0}ms.");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# rabbitmq-dotnet-client-860
+https://github.com/rabbitmq/rabbitmq-dotnet-client/issues/860
